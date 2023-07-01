13889570faa8f1b24564939bf3f5e1aef4cc3645
João Lee <joaumlimaum@gmail.com>
2023-07-01 14:44:47 -0300

    Requisito 3 parcial - passando 4 subrequisitos

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,33 @@
+import csv
+
+from src.models.ingredient import Ingredient
+from src.models.dish import Dish
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.data = source_path
+        self.created_dishes = set()
+        self.dishes = self.create_dishes(self.data)
+
+    def create_dishes(self, file_path):
+        try:
+            with open(file_path, "r", encoding="utf-8") as file:
+                _, *file_lines = csv.reader(file, delimiter=",")
+
+                for index in range(0, len(file_lines)):
+                    for dish_name, price, ingredient, quantity in file_lines:
+                        new_dish = Dish(dish_name, float(price))
+                        new_dish.add_ingredient_dependency(
+                            Ingredient(ingredient), int(quantity)
+                        )
+                        # new_dish.recipe
+                        self.created_dishes.add(new_dish)
+                return self.created_dishes
+        except:
+            FileNotFoundError()
+
+
+new_menu = MenuData("data/menu_base_data.csv")
+print(new_menu.dishes)
