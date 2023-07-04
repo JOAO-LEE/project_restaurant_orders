4c4bd97f092ed0ca907e2416f67a80b1b1626a96
João Lee <joaumlimaum@gmail.com>
2023-07-04 14:07:35 -0300

    Requisito 3 agora concluído

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,32 +1,33 @@
 import csv
 
-from src.models.ingredient import Ingredient
 from src.models.dish import Dish
+from src.models.ingredient import Ingredient
 
 
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
         self.data = source_path
-        self.created_dishes = set()
+        self.new_dish = dict()
         self.dishes = self.create_dishes(self.data)
 
     def create_dishes(self, file_path):
         try:
             with open(file_path, "r", encoding="utf-8") as file:
-                _, *file_lines = csv.reader(file, delimiter=",")
+                rows = csv.DictReader(file, delimiter=",")
+                dishes = list(rows)
 
-                for index in range(0, len(file_lines)):
-                    for dish_name, price, ingredient, quantity in file_lines:
-                        new_dish = Dish(dish_name, float(price))
-                        new_dish.add_ingredient_dependency(
-                            Ingredient(ingredient), int(quantity)
-                        )
-                        # new_dish.recipe
-                        self.created_dishes.add(new_dish)
-                return self.created_dishes
-        except:
-            FileNotFoundError()
+                for food in dishes:
+                    if food["dish"] not in self.new_dish:
+                        dish = Dish(food["dish"], float(food["price"]))
+                        self.new_dish[food["dish"]] = dish
+                    self.new_dish[food["dish"]].add_ingredient_dependency(
+                        Ingredient(food["ingredient"]),
+                        int(food["recipe_amount"]),
+                    )
+                return set(self.new_dish.values())
+        except FileNotFoundError:
+            raise FileNotFoundError()
 
 
 new_menu = MenuData("data/menu_base_data.csv")
